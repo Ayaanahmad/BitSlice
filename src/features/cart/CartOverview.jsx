c01a9636cb3d4742a4f3b1409cb54f3b877e0b52
unknown <ayaanahmad2699@gmail.com>
2025-07-19 14:00:06 +0530

    Third commit

--- src/features/cart/CartOverview.jsx
+++ src/features/cart/CartOverview.jsx
@@ -11,7 +11,7 @@ function CartOverview() {
 
   console.log(totalCartQuantity);
   return (
-    <div className="flex items-center justify-between bg-stone-800 px-4 py-4 text-sm text-stone-200 uppercase sm:px-6 md:text-base transition-all">
+    <div className=" fixed bottom-0 w-full left-0 flex items-center justify-between bg-stone-800 px-4 py-3 text-sm text-stone-200 uppercase sm:px-6 md:text-base transition-all">
       <p className="space-x-4 font-semibold text-stone-300 sm:space-x-6">
         <span>{totalCartQuantity} pizzas</span>
         <span>{formatCurrency(totalCartPrice)}</span>
