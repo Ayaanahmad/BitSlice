c9f00d5c31dfba192fa94ed97936cd174bc1a2e4
unknown <ayaanahmad2699@gmail.com>
2025-07-26 13:35:31 +0530

    final commit

--- src/ui/PageLoader.jsx
+++ src/ui/PageLoader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const PageLoader = () => {
   return (
     <div className="absolute inset-0 bg-slate-200/20 backdrop-blur-sm flex items-center justify-center">
-      <div className="loader"></div>;
+      <div className="loader"></div>
     </div>
   );
 };
